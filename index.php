6e5e86a38952e1f4964540d4c07865e27e6df554
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-03 08:53:13 -0800

    Update index.php
    Updated the index.php to fix from botched config assignment

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 <!-- START RIGHT COL -->
 <aside>
-	<h2>Hobbies</h2>
+<h2>Hobbies</h2>
 	<img src="images/tablet.jpg" class="tablet" alt="Abby's begging for Starbucks" />
 	<p>Since Covid-19 became a reality here in the Seattle area, almost before the rest of the country, I have found a love of gardening. Maybe gardening is the wrong word, I haven’t actually planted nor grown any plants. In March an amazing friend practically dragged me to the <a href="https://beaconfoodforest.org/">Beacon Food Forest</a> because “dirts good for you,” as she said. After helping weed her p-peach several times, I began helping weed with the Beacon Food Forest community areas and I love it! With the restrictions of gatherings, Beacon Food Forest was hurting with not enough volunteers to maintain it’s grounds. Usually once a month they’d have do what’s called a work party and invite the members of the community to come help weed, water and help with other tasks. These parties typically consisted of 100 plus volunteers and without these work parties, the Food Forest was beginning to get overgrown.</p>
 	<p>Maybe gardening is the wrong term. Perhaps hobby weeder is more appropriate.</p>
