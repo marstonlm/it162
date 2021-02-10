1d2de4e12724391864c1d95edf26f93a9e867383
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-10 10:56:52 -0800

    updated big head.php
    Updated the big header.php with provided from instructiors that was in the idex.html file

--- big/includes/header.php
+++ big/includes/header.php
@@ -1,38 +1,35 @@
 <?php include 'portal-config.php'?>
-<!DOCTYPE html>
-<html lang="en">
-<head> 
-<title><?=$title?></title>
-<meta name="viewport" content="width=device-width" />
-<meta name="robots" content="noindex,nofollow" />
-<meta charset="utf-8">
-<script src="https://kit.fontawesome.com/902b4b52d9.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="css/nav.css" />
-<link rel="stylesheet" href="css/portal.css" />
-<link rel="stylesheet" href="css/form.css" />
-</head>
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>MenuMaker</title>
 
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="styles.css">
+</head>
 <body>
-<!-- START WRAPPER -->
-<main class="wrapper">
-<header>
-  <h1><i class="logo fa <?=$logo?>"<?=$logo_color?>></i>Luann's IT162 Portal</h1>
-  <nav>
-    <ul class="topnav" id="myTopnav">
-      <?=makeLinks($nav1)?>
-<!-- START COMMENT OUT NAV
-      <li><a href="index.php" class="selected">Welcome</a></li>
-      <li><a href="big/index.php">Big</a></li>
-      <li><a href="aia.php">AIA</a></li>
-      <li><a href="flowchart.php">Flowchart/Layout</a></li>
-      <li><a href="fp/index.php">Final Project</a></li>
-      <li><a href="contactme.php">Contact Luann</a></li>
-END COMMENT OUT NAV -->
-      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-    </ul>
-  </nav>
-</header>
-        
-<!-- START LEFT COL -->
-<section>
- <h2 class="pageID"><?=$pageID?></h2>
+
+<div id="cssmenu">
+  <ul>
+     <li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> WEB120</span></a></li>
+     <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> BIG</span></a></li>
+     <li><a href="flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
+     <li><a href="galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
+     <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
+        <ul>
+           <li><a href="calendar.php"><span><i class="fa fa-fw fa-calendar"></i> Calendar</span></a></li>
+           <li><a href="map.php"><span><i class="fa fa-fw fa-map-o"></i> Map</span></a></li>
+           <li><a href="youtube.php"><span><i class="fa fa-fw fa-youtube-square"></i> YouTube</span></a></li>
+        </ul>
+     </li>
+     <li><a href="shoppingcarts.php"><span><i class="fa fa-fw fa-shopping-basket"></i> Shopping Carts</span></a></li>
+     <li><a href="siteapp.php"><span><i class="fa fa-fw fa-html5"></i> Site vs App</span></a></li>
+     <li><a href="webcam.php"><span><i class="fa fa-fw fa-eye"></i> Web Cam</span></a></li>
+  </ul>
+</div>
