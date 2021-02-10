b078f79a6f8a11498cc63193339594e772d96e8b
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-10 10:58:32 -0800

    Created responsivemobile.php
    Created responsivemobile.php file in the big big folder.

--- big/responsivemobile.php
+++ big/responsivemobile.php
@@ -1,4 +1,4 @@
-<?php include "../includes/header.php";?>
+<?php include "includes/header.php";?>
 
 
 
@@ -13,4 +13,4 @@
 </aside>
 <!-- END RIGHT COL -->
 
-<?php include "../includes/footer.php";?> 
+<?php include "includes/footer.php";?> 
