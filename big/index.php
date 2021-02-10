c7df945d3bd87281abfcf8f05e4baae66f2c9cb1
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-10 10:10:14 -0800

    Added text to the big/index.php file
    text is showing on the page. Need to create navagation.

--- big/index.php
+++ big/index.php
@@ -1,4 +1,16 @@
-<?php include "includes/header.php";?>
-<h1>Big</h1>
- 
-<?php include "includes/footer.php";?>
+<?php include "../includes/header.php";?>
+
+
+
+	<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+<p> in the works
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+<p>in the works</p>
+</aside>
+<!-- END RIGHT COL -->
+
+<?php include "../includes/footer.php";?> 
