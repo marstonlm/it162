9736e9f80fdff81743dc9f808d593c81289ab1d0
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-10 11:13:14 -0800

    Update big header
    Page title is displying

--- big/includes/header.php
+++ big/includes/header.php
@@ -5,13 +5,15 @@
    <meta charset='utf-8'>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title><?$title?></title>
+   <title>Luann's Big Project</title>
 
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
    <script src="script.js"></script>
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-   <link rel="stylesheet" href="css/styles.css">
+   <link rel="stylesheet" href="css/nav.css" />
+   <link rel="stylesheet" href="css/styles.css" />
+   <link rel="stylesheet" href="css/form.css" />
 </head>
 <body>
 
