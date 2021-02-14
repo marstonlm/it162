d8ac0e08dbb148d8e30202b8c76fc2b4c9354354
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-14 11:16:49 -0800

    Start of BIG w/ class instruction videos
    Start of updating code for BIG project based on pre-recorded class videos.

--- big/index.php
+++ big/index.php
@@ -1,6 +1,84 @@
-<?php include "includes/header.php";?>
 
-	<p>Is this section working?</p>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+<title>Luann's IT162 Title Page</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=aQDM5GP8EvrASakFUQW-9JwoTL1qEse8SCxLeHOttjjnpXRD8DDNhYyqLPX-bHZJnoxvUemPJWR1feHCsC63Gg" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="http://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cDovL3d3dy5sdWFubm1hcnN0b24uY29tL2l0MTYyL2luZGV4LnBocA"/><script src="https://kit.fontawesome.com/902b4b52d9.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/form.css" />
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<main class="wrapper">
+<header>
+  <h1><i class="logo fa fa-home"></i>Luann's IT162 Portal</h1>
+  <nav>
+    <ul class="topnav" id="myTopnav">
+      <li><a class="selected" href="index.php">Welcome</a></li>
+<li><a href="big/index.php">Big</a></li>
+<li><a href="aia.php">AIA</a></li>
+<li><a href="flowchart.php">Flowchart  Layout</a></li>
+<li><a href="fp/index.php">Final Project</a></li>
+<li><a href="contactme.php">Contact Luann</a></li>
+<!-- START COMMENT OUT NAV
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart/Layout</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contactme.php">Contact Luann</a></li>
+END COMMENT OUT NAV -->
+      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+</header>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+	<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+	<img src="images/desktop.jpg" class="desktop" alt="Abby in bed" />
+	<img src="images/phone.jpg" class="phone" alt="Abby eating puppuccino" />
+	<p>Hi. Here's a little about me. Starting with the possibility I may be an optimist.</p>
+	<p>2020 has been a rough year for most of us however, it’s been one of the best years. It was because of Covid I met the person who would become not only my best friend but also my spouse. That’s the optimist in me.</p>
+	<p>Also, I'm an animal lover. My home has four furbabies, two cats and two dogs. Between them, never a dull moment. To get any class work done I have to leave the apartment or hide in the bedroom! Love them all. The one in the photo is Abby, a three year old Belgian Malinois mix adopted from the <a href="https://www.kitsap-humane.org/">Kitsap humane society.</a>  Honestly she's the reason I've got to hide, never runs out of energy.</p>
 </section>
- <!-- END SECTION -->
-<?php include "includes/footer.php";?>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+<h2>Hobbies</h2>
+	<img src="images/tablet.jpg" class="tablet" alt="Abby's begging for Starbucks" />
+	<p>Since Covid-19 became a reality here in the Seattle area, almost before the rest of the country, I have found a love of gardening. Maybe gardening is the wrong word, I haven’t actually planted nor grown any plants. In March an amazing friend practically dragged me to the <a href="https://beaconfoodforest.org/">Beacon Food Forest</a> because “dirts good for you,” as she said. After helping weed her p-peach several times, I began helping weed with the Beacon Food Forest community areas and I love it! With the restrictions of gatherings, Beacon Food Forest was hurting with not enough volunteers to maintain it’s grounds. Usually once a month they’d have do what’s called a work party and invite the members of the community to come help weed, water and help with other tasks. These parties typically consisted of 100 plus volunteers and without these work parties, the Food Forest was beginning to get overgrown.</p>
+	<p>Maybe gardening is the wrong term. Perhaps hobby weeder is more appropriate.</p>
+</aside>
+<!-- END RIGHT COL -->
+
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2018 - 2021 by <a href="contactme.php" target="_blank">Luann Marston</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+</main>
+<!-- END WRAPPER -->
+
+<!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+  <script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+     </script>
+</body>
+</html>
