7555f7c30dad20f81354725ae36119cc1e57742d
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-16 11:21:51 -0800

    Update BIG /big.css & index.php

--- big/index.php
+++ big/index.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html>
 <head>
-	<title>Web Dev Examples by Sara Newman</title>
+	<title>L. Marston's IT162 BIG Project</title>
 	<meta name="viewport" content="width=device-width" />
 	<meta name="robots" content="noindex,nofollow" />
 	<meta charset="utf-8">
@@ -19,10 +19,10 @@
 	<!-- START Wrapper -->
 	<div class="wrapper">
 		<header>
-			<h1><a href="index.php"><i class="logo fa fa-home"></i>Sara Newman's Web Dev Examples</a></h1>
+			<h1><a href="index.php"><i class="logo fa fa-home"></i>BIG Project by Luann Marston</a></h1>
 			<nav id="cssmenu">
 				<ul>
-					<li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> WEB120 Portal</span></a></li>
+					<li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> IT162 Portal</span></a></li>
 					<li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> Home</span></a></li>
 					<li><a href="flexbox.php"><span>Flexbox</span></a></li>
 					<li><a href="galleries.php"><span>Galleries</span></a></li>
@@ -33,8 +33,6 @@
 							<li><a href="youtube.php"><span>YouTube</span></a></li>
 						</ul>
 					</li>
-					<li><a href="parallax.php"><span>Parallax</span></a></li>
-					<li><a href="shoppingcarts.php"><span>Shopping Carts</span></a></li>
 					<li><a href="siteapp.php"><span>Site vs App</span></a></li>
 					<li><a href="webcam.php"><span>Web Cam</span></a></li>
 				</ul>
@@ -58,7 +56,7 @@
 		<!-- END RIGHT COL -->
 		<!-- START Footer -->
 		<footer>
-			<p><small>&copy; 2016 - 2021 by <a href="#" target="_blank">Sara Newman</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+			<p><small>&copy;<?=date('Y')?> <a href="#" target="_blank">Luann Marston</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
 		</footer>
 		<!-- END Footer --> 
 	</div>
