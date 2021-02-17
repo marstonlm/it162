42d0bfbd3b9cb9f50203501796d982bf557ebab3
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-17 09:28:49 -0800

    Created Includes
    Cut up the index.php to create header.php and footer.php
    Created big-config.php

--- big/index.php
+++ big/index.php
@@ -1,65 +1,14 @@
+<?php include "includes/header.php";?>
+<p>Develop and place an updated version of the "multiple.php" contact form (located in your web120 includes folder) on this main page. Adjust its fields to ask the basic questions you'd always need to ask any client to help determine if you can help them with their website needs.</p>
+<form>add multiple.php client form here</form>
+</section>
+<!-- END LEFT COL -->
 
-<!doctype html>
-<html>
-<head>
-	<title>L. Marston's IT162 BIG Project</title>
-	<meta name="viewport" content="width=device-width" />
-	<meta name="robots" content="noindex,nofollow" />
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="css/nav.css" />
-	<link rel="stylesheet" href="css/big.css" />
-	<link rel="stylesheet" href="css/form.css" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
-	<script src="js/script.js"></script>
-</head>
-
-<body>
-	<!-- START Wrapper -->
-	<main class="wrapper">
-		<header>
-			<h1><a href="index.php"><i class="logo fa fa-home"></i>BIG Project by Luann Marston</a></h1>
-			<nav id="cssmenu">
-				<ul>
-					<li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> IT162 Portal</span></a></li>
-					<li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> Home</span></a></li>
-					<li><a href="flexbox.php"><span>Flexbox</span></a></li>
-					<li><a href="galleries.php"><span>Galleries</span></a></li>
-					<li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
-						<ul>
-							<li><a href="map.php"><span>Map</span></a></li>
-							<li><a href="calendar.php"><span>Calendar</span></a></li>
-							<li><a href="youtube.php"><span>YouTube</span></a></li>
-						</ul>
-					</li>
-					<li><a href="siteapp.php"><span>Site vs App</span></a></li>
-					<li><a href="webcam.php"><span>Web Cam</span></a></li>
-				</ul>
-			</nav>
-		</header>
-
-		<!-- START LEFT COL -->
-		<section>
-			<h2 class="pageID">Client Questionnaire</h2>
-			<p>Develop and place an updated version of the "multiple.php" contact form (located in your web120 includes folder) on this main page. Adjust its fields to ask the basic questions you'd always need to ask any client to help determine if you can help them with their website needs.</p>
-			<form>add multiple.php client form here</form>
-		</section>
-		<!-- END LEFT COL -->
-
-		<!-- START RIGHT COL -->
-		<aside>
-			<h3>Basic Website Design Cycle</h3>
-			<p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
-			<p>Here's a website to help get you started, <a href="https://business.tutsplus.com/articles/how-to-extract-the-facts-with-a-web-design-client-questionnaire--fsw-1830" target="_top">https://business.tutsplus.com/articles/...</a></p>
-		</aside>
-		<!-- END RIGHT COL -->
-		<!-- START Footer -->
-		<footer>
-			<p><small>&copy;<?=date('Y')?> <a href="#" target="_blank">Luann Marston</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
-		</footer>
-		<!-- END Footer --> 
-	</main>
-	<!-- END Wrapper -->
-</body>
-</html>
+<!-- START RIGHT COL -->
+<aside>
+	<h3>Basic Website Design Cycle</h3>
+	<p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
+	<p>Here's a website to help get you started, <a href="https://business.tutsplus.com/articles/how-to-extract-the-facts-with-a-web-design-client-questionnaire--fsw-1830" target="_top">https://business.tutsplus.com/articles/...</a></p>
+</aside>
+<!-- END RIGHT COL -->
+<?php include "includes/footer.php";?>
