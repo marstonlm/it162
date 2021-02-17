047dde35ec43b881a90f0a3dccf8834ded27e8ab
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-17 10:30:54 -0800

    Updated big/flexbox
    Modified file. Displays properly on page.

--- big/flexbox.php
+++ big/flexbox.php
@@ -1,13 +1,18 @@
 <?php include "includes/header.php";?>
-<p>Flexbox section</p>
+<p>Copy and Paste my Flexbox reseach and citations to this page when ready.
+
+</p>
 
 </section>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
 <aside>
-	<h3>Flexbox aside</h3>
-	<p>Flexbox aside</p>
+	<h3>Citations</h3>
+	<p>Citations could go here</p>
+	<p>Citations could go here</p>
+	<p>Citations could go here</p>
+	<p>Citations could go here</p>
 </aside>
 <!-- END RIGHT COL -->
 <?php include "includes/footer.php";?>
