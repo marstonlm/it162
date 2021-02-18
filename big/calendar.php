aa92a1a1f7d7cb8d67efedcc881e5a72e701887f
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-17 16:45:39 -0800

    Updated calendar.php
    Created an event in the my seattlecentral google calendar. Embeded the month view. Agenda view still needs work.

--- big/calendar.php
+++ big/calendar.php
@@ -1,14 +1,20 @@
 <?php include "includes/header.php";?>
-	
-	<div class="embed-container">
-		Iframe in a responsive calender and it will have an event linked to a supporting website.
+
+<!-- Show/Hide MONTH View Calendar based on resolution -->	
+	<div class='month-view embed-container'><iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%233b1f2b&ctz=America%2FLos_Angeles&src=Y19lODBudWgxdTdsbW80ajZmcThoZmMwcnJwb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA&showTitle=0&mode=MONTH' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe></div>
+
+<!-- Show/Hide AGENDA View Calendar based on resolution -->
+<!--
+	<div class='agenda-view embed-container'>
+		<iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%233b1f2b&ctz=America%2FLos_Angeles&src=Y19lODBudWgxdTdsbW80ajZmcThoZmMwcnJwb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA&showTitle=0&mode=AGENDA' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
 	</div>
+-->
 </section>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
 <aside>
-	<h3>Event Info</h3>
+	<h3>50th Annual Northwest Folklife Festival</h3>
 	<p>Put web address and event info here</p>
 </aside>
 <!-- END RIGHT COL -->
