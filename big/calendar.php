8d0a3fdcf8017c949d43b130f14e974af07efdc5
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-19 10:07:49 -0800

    Update calendar portion
    calendar.php fixed the responsive for the Phat Phone
    
    big.css --> fixed the code for the agenda view
    
    live page is responsive and displays properly

--- big/calendar.php
+++ big/calendar.php
@@ -1,9 +1,15 @@
 <?php include "includes/header.php";?>
 
 <!-- Show/Hide MONTH View Calendar based on resolution -->	
-	<div class='month-view embed-container'><iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%233b1f2b&ctz=America%2FLos_Angeles&src=Y19lODBudWgxdTdsbW80ajZmcThoZmMwcnJwb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA&showTitle=0&mode=MONTH' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe></div>
+	<div class='month-view embed-container'>
+		<iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%233b1f2b&ctz=America%2FLos_Angeles&src=Y19lODBudWgxdTdsbW80ajZmcThoZmMwcnJwb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA&showTitle=0&mode=MONTH' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
+	</div>
 
 <!-- Show/Hide AGENDA View Calendar based on resolution -->
+		<div class='embed-container agenda-view'>
+			<iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%233b1f2b&ctz=America%2FLos_Angeles&src=Y19lODBudWgxdTdsbW80ajZmcThoZmMwcnJwb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA&showTitle=0&mode=AGENDA' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
+		</div>
+
 <!--
 	<div class='agenda-view embed-container'>
 		<iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%233b1f2b&ctz=America%2FLos_Angeles&src=Y19lODBudWgxdTdsbW80ajZmcThoZmMwcnJwb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%238E24AA&showTitle=0&mode=AGENDA' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'></iframe>
