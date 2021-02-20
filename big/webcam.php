e1779a87c1e905beae0f6ebe19a7845521d5cfd8
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-20 11:45:29 -0800

    Update webcam.php
    DOT Traffic camera is properly refreshing every 30 seconds

--- big/webcam.php
+++ big/webcam.php
@@ -1,16 +1,17 @@
 <?php include "includes/header.php";?>
 	
 	<div class="embed-container">
-		Iframe in a responsive live YouTube video - in video they did live earth and kitten academy.
+		<iframe src='https://www.youtube.com/embed/uEeI6Y-SCfQ' frameborder='0' allowfullscreen title="Kitten Academy Live stream"></iframe>	
 	</div>
 </section>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
 <aside>
-	<h3>Nave of Live DOT Camera</h3>
+	<h3>Name of Live DOT Camera</h3>
 	<div class="embed-container">
-		Iframe in a refreshing web page. See video 2 in lesson 1.
+		<meta http-equiv="refresh" content="30">
+		<iframe src='https://images.wsdot.wa.gov/sc/012vc15095.jpg' title="Traffic Cam of White Pass" frameborder='0' allowfullscreen></iframe>	
 	</div>
 </aside>
 <!-- END RIGHT COL -->
