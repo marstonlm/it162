01e8ec5d30a4d8b62d5cb776c7706b4d3f7fed8a
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-02-21 08:32:06 -0800

    Upload files for form
    Created files:
    contactbig.php
    contact_include.php
    multiple.php
    These are copied from 162 Portal site and renamed for BIG project.

---
+++ big/contactbig.php
@@ -0,0 +1,23 @@
+<?php include "includes/header.php";?>
+<?php
+/*
+ * Below are 2 different forms to be re-used       
+ * 
+ * Only use one at a time, comment out the other!       
+ *
+ */
+
+include 'includes/big-contact_include.php'; #site keys & code here
+
+$toAddress = "luann.webster@seattlecentral.edu";  //place your/your client's email address here
+$toName = "Luann Marston"; //place your client's name here
+$website = "Luann's BIG Project";  //place NAME of your client's website
+
+//echo loadContact('simple.php');#demonstrates a simple contact form
+echo loadContact('multiple.php');#demonstrates multiple form elements
+
+?> 
+        <div>
+        </div>
+</section>
+<?php include "includes/footer.php";?>
