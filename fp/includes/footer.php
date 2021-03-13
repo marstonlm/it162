b4f95bf01b5a65f49d1b695645316838b84bcd78
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-03-13 14:13:54 -0800

    Create .php files
    Took the template.html and turned it into three .php files: includes/header.php, includes/footer.php and template.php

---
+++ fp/includes/footer.php
@@ -0,0 +1,68 @@
+
+  <!-- START Footer -->
+  <footer>
+    <div class="container">
+      <div class="footer-logo"><a href="template.html"><img src="images/site_logo.jpg" class="logo responsive"></a>
+        </div>
+      <!-- place social media icons here -->
+      <div class="social row">
+        <div class="column"><img src="images/facebook.jpg" class="facebook icon"></div>
+        <div class="column"><img src="images/instagram.jpg" class="instagram icon"></div>
+        <div class="column"><img src="images/linkedin.jpg" class="linkedin icon"></div>
+        <div class="column"><img src="images/twitter.jpg" class="twitter icon"></div>
+      </div>
+    </div>
+    <div class="container footer-lower">
+      <div class="site-map">
+        <table>
+          <tr>
+            <th><a href="services.php">Services</a></th>
+            <th><a href="work.php">Work</a></th>
+            <th><a href="us.php">Us</a></th>
+            <th><a href="blog.php">Blog</a></th>
+            <th><a href="contact.php" class="th-contact">Contact</a></th>
+          </tr>
+<!-- Maybe add more links to site map
+        <tr>
+          <td>Software</td>
+          <td>Portifolio</td>
+          <td>Our Story</td>
+          <td> </td>     
+          <td>Contact Form</td>    
+        </tr>
+        <tr>
+          <td>Mobile Apps</td>
+          <td>Testamonials</td>
+          <td>The Team</td>
+          <td> </td>
+          <td>Career Oppertunities</td>
+        </tr>
+        <tr>
+          <td>Database</td>
+        </tr>
+      -->
+    </table>
+  </div>
+  <div class="copyright"><p><small>&copy; <script>document.write(new Date().getFullYear())</script> by <a href="contactme.php" target="_blank">A4 Loop</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+  </div>
+</div>
+</footer>
+<!-- END Footer --> 
+
+</main>
+<!-- END WRAPPER -->
+
+<!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+<script>
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+</script>
+</body>
+</html>
---
+++ fp/template.php
@@ -0,0 +1,12 @@
+<?php include "includes/header.php";?>
+      <div class="content">
+        <h2>Balinese tomcat egyptian mau</h2>
+        <p>Cat ipsum dolor sit amet, turkish angora maine coon jaguar so himalayan. Donskoy kitty. Cornish rex sphynx mouser. Bobcat turkish angora jaguar donskoy maine coon tabby burmese. Thai leopard. Grimalkin himalayan so bengal. Singapura bobcat and siberian and egyptian mau for russian blue. Jaguar malkin yet russian blue devonshire rex so panther. Himalayan bombay savannah yet cornish rex puma, scottish fold. Grimalkin siberian yet jaguar, or leopard yet american bobtail. Havana brown savannah but donskoy..</p>
+      </div>
+    </section>
+    <!-- SECTION -->
+
+    <!-- END MAIN CONTAINT -->
+  </div>
+
+  <?php include "includes/footer.php";?>
