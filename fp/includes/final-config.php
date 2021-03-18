a4ef4bfc1474485dfae18f19c30d1cd864121efd
marstonlm <72314098+marstonlm@users.noreply.github.com>
2021-03-18 09:05:34 -0700

    Update final-config.php
    Removed $logo and $logo_color from file.
    Added $pagePic to each page that displays a specific pic meant for each page.

--- fp/includes/final-config.php
+++ fp/includes/final-config.php
@@ -18,133 +18,86 @@ define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 //echo THIS_PAGE;
 
 //die;
-
+// $pagePic = 'images/pade_id.jpg';
 
 switch(THIS_PAGE){
 	case 'index.php':
-		$title = " ";
-		$logo = 'fa-home';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$title = " A4 Loop";
+		$pagePid = '';
 		$pageID = ' ';
 	break;
 
 	case 'services.php':
 		$title = " Services";
+		$pagePic = 'images/services_pic.jpg';
 		$pageID = 'Services';
 	break;
 
 	case 'software.php':
 		$title = " Software";
+		$pagePic = 'images/software_pic.jpg';
 		$pageID = 'Software';
 	break;
 
 	case 'mobile.php':
 		$title = " Mobile Apps";
-		$logo = 'fa-universal-access';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$pagePic = 'images/mobile_pic.jpg';
 		$pageID = 'Mobile Apps';
 	break;
 
 	case 'database.php':
 		$title = " Database";
-		$logo = 'fa-universal-access';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$pagePic = 'images/database_pic.jpg';
 		$pageID = 'Database';
 	break;
 
 	case 'portfolio.php':
 		$title = " Portfolio";
+		$pagePic = 'images/portfolio_pic.jpg';
 		$pageID = 'Portfolio';
 	break;
 
 	case 'about.php':
 		$title = " about";
-		$logo = 'fa-pencil-square-o';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$pagePic = 'images/about_pic.jpg';
 		$pageID = 'About';
 	break;
 
 	case 'ourstory.php':
 		$title = " Our Story";
-		$logo = 'fa-pencil-square-o';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$pagePic = 'images/ourstory_pic.jpg';
 		$pageID = 'Our Story';
 	break;
 
 	case 'team.php':
 		$title = " The Team";
-		$logo = 'fa-pencil-square-o';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$pagePic = 'images/team_pic.jpg';
 		$pageID = 'The Team';
 	break;
 
-	case 'careers.php':
+	case 'career.php':
 		$title = " Careers";
-		$logo = 'fa-pencil-square-o';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
+		$pagePic = 'images/careers_pic.jpg';
 		$pageID = 'Career Oppertunities';
 	break;
 
 	case 'blog.php':
 		$title = "Blog";
-		$logo = 'fa-pencil-square-o';
+		$pagePic = 'images/blog_pic.jpg';
 		$pageID = 'Blog';
 	break;
 
 	case 'contact.php':
-		$title = " Contact";
-		$logo = 'fa-paper-plane-o';
-		$logo_color = ' style="color:rgba(128,26,134,.15)"';
-		$pageID = 'Contact';
+		$title = " Contact Us";
+		$pagePic = 'images/contact_pic.jpg';
+		$pageID = 'Contact Us';
 	break; 
 
 	default:
 		$title = THIS_PAGE;
-		$logo = ''; //no icon by default
-		$logo_color = ''; // make logo_color an empty string by default
+		$pagePic = 'images/page_id.jpg';
 		$pageID = ''; //no icon by default
 }
 
-// arry for navigation
 
-$nav1['index.php'] = "";
-$nav1['services.php'] = "Services";
-$nav1['portfolio.php'] = "Portfolio";
-$nav1['about.php'] = "About";
-$nav1['blog.php'] = "BLOG";
-$nav1['contact.php'] = "Contact";
-
-
-/* 
-*function for nav links
-*hightlight current page
-
-  <li><a class="selected" href="index.php"></a></li>
-  <li><a href="services.php">Services</a></li>
-  <li><a href="portfolio.php">Portfolio</a></li>
-  <li><a href="about.php">About</a></li>
-  <li><a href="blog.php">Blog</a></li>
-  <li><a href="contact.php">Contact</a></li>
-*/
-
-/*
-
-function makeLinks($linkArray)
-{
-    $myReturn = '';
-
-    foreach($linkArray as $url => $text)
-    {
-        if($url == THIS_PAGE)
-        {//selected page - add class reference
-	    	$myReturn .= '<li><a class="selected" href="' . $url . '">' . $text . '</a></li>' . PHP_EOL;
-    	}else{
-	    	$myReturn .= '<li><a href="' . $url . '">' . $text . '</a></li>'  . PHP_EOL;
-    	}    
-    }
-      
-    return $myReturn;   
-} 
-
-*/
 ?>
